1702a1d9b57a62c1083541f67b6cc0d139dc1a4f
sandaruny <sandaru.ny@gmail.com>
2016-11-16 11:54:12 +0530

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './src/helloWorld/view/hello-world.jsx'
+
+ ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
+
+
